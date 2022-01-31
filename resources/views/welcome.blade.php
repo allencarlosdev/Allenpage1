933a4358cbfc391c11b4c4f0b3eae5a4c6dcaa42
CarlosAllen93 <allencarlosdev@gmail.com>
2022-01-31 18:49:53 -0400

    adding the wave tag and changing its color

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -24,8 +24,8 @@
             <h1> Share your Photo Edits </h1>
             <h2>AllenCarlosDesigner</h2>
         </section>
-        <div style="height: 150px; overflow: hidden;"><svg viewBox="0 0 500 150" preserveAspectRatio="none" style="height: 100%; width: 100%;">
-                <path d="M0.00,49.98 C149.99,150.00 349.20,-49.98 500.00,49.98 L500.00,150.00 L0.00,150.00 Z" style="stroke: none; fill: #08f;"></path>
+        <div class="wave" style="height: 150px; overflow: hidden;"><svg viewBox="0 0 500 150" preserveAspectRatio="none" style="height: 100%; width: 100%;">
+                <path d="M0.00,49.98 C149.99,150.00 349.20,-49.98 500.00,49.98 L500.00,150.00 L0.00,150.00 Z" style="stroke: none; fill: #3e0796;"></path>
             </svg></div>
     </header>
     <main>
