8414102483e5529acd4da455815658f9c487f739
CarlosAllen93 <allencarlosdev@gmail.com>
2022-02-07 23:08:44 -0400

    Adding responsive design part 2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -35,11 +35,6 @@
                 <li><a href="#services">Services</a></li>
                 <li><a href="#contact">Contact</a></li>
             </ul>
-            {{-- <a href="#home">Home</a>
-            <a href="#gallery">Gallery</a>
-            <a href="#about">About</a>
-            <a href="#services">Services</a>
-            <a href="#contact">Contact</a> --}}
         </nav>
         <section class="header-text">
             <h1> Share your Photo Edits </h1>
@@ -154,8 +149,7 @@
             <div class="content-footer">
                 <ul class="socials">
                     <li><a href="https://github.com/CarlosAllen93" target="_blank"><i class="fa fa-github"></i></a></li>
-                    <li><a href="https://stackoverflow.com/users/16209550/carlos-allen" target="_blank"><i
-                                class="fa fa-stack-overflow"></i></a></li>
+                    <li><a href="https://stackoverflow.com/users/16209550/carlos-allen" target="_blank"><i class="fa fa-stack-overflow"></i></a></li>
                     <li><a href="https://www.linkedin.com/in/carlos-wilder-allen-pe%C3%B1a-7a7699123/" target="_blank"><i
                                 class="fa fa-linkedin"></i></a></li>
                     <li class="li-icon"><a href="https://www.sololearn.com/profile/20725585" target="_blank"><i class="fab fa-stripe-s"></i></a></li>
